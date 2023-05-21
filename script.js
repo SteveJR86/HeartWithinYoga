6afa7aa05cb46985d4cb6674f02412311ec1204b
SteveJR86 <15844719+SteveJR86@users.noreply.github.com>
2023-05-21 20:54:18 +0100

    updated to address some lighthouse report issues

--- script.js
+++ script.js
@@ -1,14 +1,17 @@
 const menu = document.getElementById('menu');
 const menuIcon = document.getElementById('menuIcon');
+const navBtn = document.getElementById('navBtn');
 
 function openclosemenu(){
   if(menu.classList.contains('menuShow')){
     menu.classList.remove('menuShow');
     menu.classList.add('menuHide');
     menuIcon.src = "./assets/icons/bars-solid.svg";
+    navBtn.ariaLabel = "Hide menu";
   } else {
     menu.classList.remove('menuHide');
     menu.classList.add('menuShow');
     menuIcon.src = "./assets/icons/xmark-solid.svg";
+    navBtn.ariaLabel = "Hide menu";
   }
 }
