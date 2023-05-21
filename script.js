cf807d26d15781ae8e322a32d05f7e914f45f921
SteveJR86 <15844719+SteveJR86@users.noreply.github.com>
2023-05-21 21:06:52 +0100

    javascript updated

--- script.js
+++ script.js
@@ -2,6 +2,9 @@ const menu = document.getElementById('menu');
 const menuIcon = document.getElementById('menuIcon');
 const navBtn = document.getElementById('navBtn');
 
+navBtn.addEventListener('click', openclosemenu);
+menu.addEventListener('click', openclosemenu);
+
 function openclosemenu(){
   if(menu.classList.contains('menuShow')){
     menu.classList.remove('menuShow');
@@ -12,6 +15,6 @@ function openclosemenu(){
     menu.classList.remove('menuHide');
     menu.classList.add('menuShow');
     menuIcon.src = "./assets/icons/xmark-solid.svg";
-    navBtn.ariaLabel = "Hide menu";
+    navBtn.ariaLabel = "Show menu";
   }
 }
